aca5af2a23e6aafe2d5c1b3504db3f60d887ff22
roshan786143 <roshanbabu.kng@gmail.com>
2022-11-01 15:39:42 +0530

    added cool drinks promise

--- test.js
+++ test.js
@@ -12,53 +12,35 @@ function getPost(){
 }
 
 getPost();
-let promise1;
-function createPost(post){
-    return promise1 = new Promise((resolve,reject)=>{
-        
-        posts.push(post);
-        if(posts.length==3){
-            resolve();
-        }else{
-            reject();
-        }
-    })
-}
+let createPost,deletePost;
 
-createPost({title : "post three", body : "My third post"})
-.then(()=>{
+const main = async(post)=>{
+    createPost = new Promise((resolve,reject)=>{
+        setTimeout(()=>resolve(post),3000);
+    })
+
+    deletePost = new Promise((resolve,reject)=>{
+        setTimeout(()=>resolve(post),5000);
+    })
+    let create_post = await createPost;
+    posts.push(post);
+        console.log(posts);
+        console.log(posts.length);
     console.log("hurray your new post added successfully at "+ new Date().toUTCString())
     getPost();
-})
-.catch(()=>{
-    console.log("There's something wrong with your code check it once.")
-})
 
-let promise2;
-function deletePost(){
-    
-    return promise2 = new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-        posts.pop();
-        if(posts.length==2){
-            resolve();
-        }else{
-            reject();
-        }
-    },3000);
-    })
-}
-deletePost().then(()=>{
+    let delete_post = await deletePost;
+    posts.pop();
+    console.log(posts);
+    console.log(posts.length);
     console.log("your post deleted successfully.")
     getPost();
-})
-.catch(()=>{
-    console.log("your post didn't deleted,pls.check it why it was happened.")
-})
+}
 
-Promise.all([promise1,promise2]).then(()=>{
+main({title : "post three", body : "My third post"})
+.then(()=>{
     console.log("Both operations done successfully.");
 })
 .catch(()=>{
-    console.log("Neither of the two failed to execute.")
+console.log("Neither of the two failed to execute.");
 })
